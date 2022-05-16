0d5f88b5875f9452b1eaaa2a9b07f7ed26cd8521
XIO <qq1954556630@gmail.com>
2022-05-16 19:40:05 +0800

    15.加載動畫和捕獲異常

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_ex_jdstore/config/jd_api.dart';
@@ -17,6 +18,8 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
 
+    // 下方原本請求數據的方法直接註解home_page_provider.dart處理
+
     //原本官方示範的代碼貼過來確認無誤即可先註解起來
     // NetRequest();
 
@@ -30,7 +33,7 @@ class _homePageState extends State<homePage> {
     // 最後改這樣
     // NetRequest().requestData(JdApi.HOME_PAGE).then((res) => print(res.data));
 
-
+    // 這部分必須改寫，因為要接入HomePageProvider
     // return Scaffold(
     //   appBar: AppBar(
     //     title: Text('主頁'),
@@ -44,9 +47,41 @@ class _homePageState extends State<homePage> {
       },
       child: Scaffold(
         appBar: AppBar(title: Text('首頁'),),
-        body: Consumer<HomePageProvider>(builder: (_,provider,__){
-          return Container();
-        }),
+        body: Container(
+          color: Color(0xFFf4f4f4),
+          child: Consumer<HomePageProvider>(builder: (_,provider,__){
+
+            // // 打印出加載狀態，加載前為true，加載完成為false
+            // print(provider.isLoading);
+
+            // 加載資料的時候顯示的轉圈動畫
+            if(provider.isLoading){
+              return const Center(
+                child: CupertinoActivityIndicator(),
+              );
+            }
+
+            // 捕獲異常 當無法連接後端資料時，跳出錯誤並顯示刷新按鈕
+            if(provider.isError){
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(provider.errorMsg),
+                    OutlinedButton(
+                        onPressed: (){
+                          provider.loadHomePageData();
+                        },
+                        child: Text("刷新")
+                    )
+                  ],
+                ),
+              );
+            }
+
+            return Container();
+          }),
+        ),
       ),
     );
   }
--- lib/provider/home_page_provider.dart
+++ lib/provider/home_page_provider.dart
@@ -11,27 +11,40 @@ class HomePageProvider with ChangeNotifier{
   bool isError = false;
   String errorMsg = "";
 
+  // 設置一個能暴露給外部接入的接入點
   loadHomePageData(){
     isLoading = true;
     isError = false;
     errorMsg = "";
+
+    // 向後端請求數據
     NetRequest().requestData(JdApi.HOME_PAGE).then((res){
+
+      // 請求結束後將狀態改為false
       isLoading = false;
+
       if(res.code==200){
         print(res.data);
+
+        // 將請求的json轉化成map對象
         model = HomePageModel.fromJson(res.data);
       }
+      // 這部分在這沒太多必要，故直接省略
       // else{
       //   print(res.msg);
       //   isError = true;
       //   errorMsg = res.msg!;
       // }
+
+      // 當發生任何變化時，必須要有對應的通知
       notifyListeners();
     }).catchError((error){
       print(error);
       errorMsg = error;
       isLoading = false;
       isError = true;
+
+      // 當發生任何變化時，必須要有對應的通知
       notifyListeners();
     });
   }
